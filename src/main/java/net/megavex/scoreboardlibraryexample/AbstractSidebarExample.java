aac123bd9f78eb5d131827e43fb2f80c6800db3f
VytskaLT <VytskaLT@protonmail.com>
2022-01-09 16:39:18 +0200

    update

--- src/main/java/net/megavex/scoreboardlibraryexample/AbstractSidebarExample.java
+++ src/main/java/net/megavex/scoreboardlibraryexample/AbstractSidebarExample.java
@@ -8,6 +8,8 @@ import net.megavex.scoreboardlibrary.api.sidebar.AbstractSidebar;
 import net.megavex.scoreboardlibrary.api.sidebar.line.SidebarLine;
 import net.megavex.scoreboardlibraryexample.util.ColorUtil;
 import net.megavex.scoreboardlibraryexample.util.TestTranslator;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -15,6 +17,8 @@ import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
 
 import java.awt.*;
+import java.util.Collection;
+import java.util.Locale;
 import java.util.function.Supplier;
 
 import static net.kyori.adventure.text.Component.text;
@@ -32,12 +36,12 @@ public class AbstractSidebarExample extends AbstractSidebar implements Listener 
         registerEmptyLine(0);
         SidebarLine unixLine = registerLine(1, (Supplier<Component>)
                 () -> text("Unix time: " + System.currentTimeMillis())
-                        .color(TextColor.color(Color.RED.getRGB())));
+                        .color(TextColor.color(Color.RED.getRGB()))); // ?
 
         registerEmptyLine(2);
         registerStaticLine(3, text("VeryLongLineThatShouldBeCutOffFor1.12AndBelowPlayersButNotOnNewerVersions", NamedTextColor.AQUA, TextDecoration.BOLD));
         SidebarLine flashingLine = registerLine(4, (Supplier<Component>)
-                () -> text("Line that changes colors", ColorUtil.randomNamedColor()));
+                () -> text("Line that changes colors", ColorUtil.randomNamedColor())); // ?
 
         registerEmptyLine(5);
         registerStaticLine(6, text("Identical line"));
@@ -48,14 +52,23 @@ public class AbstractSidebarExample extends AbstractSidebar implements Listener 
 
         // Do random stuff to see if everything works
         task = new BukkitRunnable() {
+
             byte tick = 0;
+            boolean flag = false;
+            boolean hidden = true;
 
             @Override
             public void run() {
                 tick++;
 
-                if (!sidebar.visible() && tick == 20) {
-                    sidebar.visible(true);
+                if (hidden && tick == 20) {
+                    if (flag) {
+                        sidebar.visible(true);
+                    } else {
+                        sidebar.addPlayers((Collection<Player>) Bukkit.getOnlinePlayers());
+                    }
+                    flag = !flag;
+                    hidden = false;
                     tick = 0;
                     return;
                 }
@@ -72,7 +85,14 @@ public class AbstractSidebarExample extends AbstractSidebar implements Listener 
 
                 if (tick == 100) {
                     tick = 0;
-                    sidebar.visible(false);
+                    if (flag) {
+                        sidebar.visible(false);
+                    } else {
+                        sidebar.removePlayers(sidebar.players());
+                    }
+
+                    hidden = true;
+
                 }
 
                 updateLine(unixLine);
